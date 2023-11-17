8e549cf3d09583035e9b79558644bc8043549ca1
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-17 15:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// Do not change the code below.
+const firstInput = prompt("Enter the first input:");
+const secondInput = prompt("Enter the second input:");
+
+const count= 2 * firstInput;
+const result = count + secondInput;
+
+alert("The final result is: " + result);
+
+
